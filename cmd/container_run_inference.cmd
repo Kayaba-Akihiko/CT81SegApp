792b5f166ed7be34699dce135e492c27cbbd8ff6
gu <gu.yi.gu4@naist.ac.jp>
2025-12-16 13:08:08 +0900

    update

--- cmd/container_run_inference.cmd
+++ cmd/container_run_inference.cmd
@@ -5,11 +5,15 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 set "CT_IMAGE_PATH=G:\projects\ct81segapp_export\input\UZU00001_CT1_low"
 set "OUTPUT_DIR=G:\projects\ct81segapp_export\output"
+:: Number of workers for reading in dicom format.
 set N_WORKERS=8
 set BATCH_SIZE=2
+:: cuda or cpu
 set DEVICE=cuda
+:: Number of GPUs if DEVICE is cuda, otherwise it has to be 1.
 set N_DEVICES=1
-set SAVE_CT=true
+:: set to "1" or "yes" or "true" to output ct (in mha format)
+set SAVE_IMAGE=false
 set DICOM_NAME_REGEX=\".*\"
 
 set DISTRO_NAME=ct81seg-ubuntu
@@ -19,15 +23,19 @@ set SCRIPT_DIR=%~dp0
 set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 set SRC_DIR=%SCRIPT_DIR%\src
 set LOG_FILE=%OUTPUT_DIR%\inference.log
-mkdir "%OUTPUT_DIR%"
-
-rem ---- default (OFF) ----
-set "SAVE_CT_ARG="
-rem ---- enable if truthy ----
-echo %SAVE_CT% | findstr /I /R "^\(1\|true\|yes\)$" >nul
-if %ERRORLEVEL%==0 (
-    set "SAVE_CT_ARG=--save_ct"
+if not exist "%OUTPUT_DIR%" (
+    mkdir "%OUTPUT_DIR%"
 )
+
+rem Debug: show raw value
+echo SAVE_IMAGE=[%SAVE_IMAGE%]
+
+echo(%SAVE_IMAGE%| findstr /I /X /C:"1" /C:"true" /C:"yes" >nul
+if %ERRORLEVEL%==0 set "SAVE_IMAGE_ARG=--save_image"
+
+echo "SAVE_IMAGE=%SAVE_IMAGE%"
+echo "SAVE_IMAGE_ARG=%SAVE_IMAGE_ARG%"
+pause
 
 rem Convert PROJECT_DIR to WSL path
 call :to_wsl_path %SRC_DIR%
@@ -40,7 +48,7 @@ set OUTPUT_DIR=%WSL_PATH%
 call :log "[Script start] %DATE% %TIME%"
 :: Record start time
 set "START_TIME=%TIME%"
-wsl -d %DISTRO_NAME% -u %WSL_USER% -- bash -lc "singularity exec --nv --nvccli --bind /mnt %SRC_DIR%/resources/py3.12-torch2.8-cu12.8_latest.sif python %SRC_DIR%/main.py --image_path %CT_IMAGE_PATH% --output_dir %OUTPUT_DIR% --dicom_name_regex %DICOM_NAME_REGEX% --n_workers %N_WORKERS% --batch_size %BATCH_SIZE% --device %DEVICE% --dist_devices %N_DEVICES% %SAVE_CT_ARG%"
+wsl -d %DISTRO_NAME% -u %WSL_USER% -- bash -lc "singularity exec --nv --nvccli --bind /mnt %SRC_DIR%/resources/py3.12-torch2.8-cu12.8_latest.sif python %SRC_DIR%/main.py --image_path %CT_IMAGE_PATH% --output_dir %OUTPUT_DIR% --dicom_name_regex %DICOM_NAME_REGEX% --n_workers %N_WORKERS% --batch_size %BATCH_SIZE% --device %DEVICE% --dist_devices %N_DEVICES% %SAVE_IMAGE_ARG%"
 :: Record end time
 set "END_TIME=%TIME%"
 call :log "[Script end] %DATE% %TIME%"
